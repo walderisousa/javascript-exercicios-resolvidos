cf959e38bd08c9751d6fdb429844d20f8d92373a
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-08 10:18:31 -0300

    Enunciado e início do código.

--- feira.js
+++ feira.js
@@ -3,3 +3,13 @@ mostre o preço da que fruta seja por quilo ou o que seja (unidade, bandeja,
 etc.), depois pergunte quantas unidades ele quer e por fim mostre o valor
 total
 */
+
+const prompt = require('prompt-sync')()
+
+console.log(`
+    + ++++++++++++++++++++++++++++++++++++++++++ +
+    +     [ATENÇÃO] PARA MAÇÃ USE --> MACA       +
+    + ++++++++++++++++++++++++++++++++++++++++++ +
+    `)
+let frutas = prompt('Digite a fruta que você deseja: ')
+
