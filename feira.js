a80eb281fb9241f0bd2c6d6ed96b29f9df3322c3
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-08 10:03:48 -0300

    First exercise added

---
+++ feira.js
@@ -0,0 +1,5 @@
+/* Crie um sistema que pede para o usuário escolher uma fruta, depois
+mostre o preço da que fruta seja por quilo ou o que seja (unidade, bandeja,
+etc.), depois pergunte quantas unidades ele quer e por fim mostre o valor
+total
+*/
