698fb49dccd4f9d2384731070663377d9ec77173
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-08 11:06:16 -0300

    declarando as constantes

--- feira.js
+++ feira.js
@@ -5,11 +5,43 @@ total
 */
 
 const prompt = require('prompt-sync')()
+const preco = {
+    maca: 6.20,
+    banana: 5.20,
+    laranja: 4.99,
+    manga: 6.99,
+    mamao: 7.50,
+    melancia: 1.59,
+    abacaxi: 3.99,
+    pera: 10.99,
+    uva: 10.99,
+    melao: 2.99,
+}
+
+const peso = {
+    maca: 0.150,
+    banana: 0.100,
+    laranja: 0.180,
+    manga: 0.400,
+    mamao: 0.350,
+    melancia: 6.000,
+    abacaxi: 1.500,
+    pera: 0.160,
+    uva: 1,
+    melao: 1.700,
+}
 
 console.log(`
     + ++++++++++++++++++++++++++++++++++++++++++ +
     +     [ATENÇÃO] PARA MAÇÃ USE --> MACA       +
+    +               PARA MAMÃO USE --> MAMAO     +
+    +               PARA MELÃO USE --> MELAO     +
     + ++++++++++++++++++++++++++++++++++++++++++ +
     `)
-let frutas = prompt('Digite a fruta que você deseja: ')
+let frutas =  prompt('Digite a fruta que você deseja: ')
 
+switch(frutas.toLowerCase){
+    case maca:
+        const preco_maca = 5.99
+        console.log('O preço da maçã está R$' + preco_maca + '/Kg')
+}
