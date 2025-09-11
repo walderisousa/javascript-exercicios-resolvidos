31fb5b7f1e1af429fdb65a2eaef890bd505cb8ab
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-11 16:23:00 -0300

    finalização feira.js

--- feira.js
+++ feira.js
@@ -3,22 +3,17 @@ mostre o preço da que fruta seja por quilo ou o que seja (unidade, bandeja,
 etc.), depois pergunte quantas unidades ele quer e por fim mostre o valor
 total
 */
+
 const prompt = require('prompt-sync')();
 
-let peso_fruta
-let preco_fruta
-let preco_total
-let peso_total
-let fruta
-let qtd
-let frutas
+let preco_total, peso_total, qtd, frutas;
 
 const preco = {
-    maca: 6.20,
-    banana: 5.20,
+    maca: 6.19,
+    banana: 5.19,
     laranja: 4.99,
     manga: 6.99,
-    mamao: 7.50,
+    mamao: 7.49,
     melancia: 1.59,
     abacaxi: 3.99,
     pera: 10.99,
@@ -35,49 +30,159 @@ const peso = {
     melancia: 6.000,
     abacaxi: 1.500,
     pera: 0.160,
-    uva: 1,
     melao: 1.700,
 }
 
 console.log(`
-    + ++++++++++++++++++++++++++++++++++++++++++ +
-    +     [ATENÇÃO] NÃO É POSSÍVEL UTILIZAR Ç    +
-    +               NEM ACENTOS, SENDO ASSIM:    +
-    +                                            +
-    +               PARA MAÇÃ USE --> MACA       +
-    +               PARA MAMÃO USE --> MAMAO     +
-    +               PARA MELÃO USE --> MELAO     +
-    + ++++++++++++++++++++++++++++++++++++++++++ +
+    + +++++++++++++++++++++++++++++++++++++++++++ +
+    +  [ATENÇÃO] NÃO É POSSÍVEL UTILIZAR Ç        +
+    +            NEM ACENTOS, SENDO ASSIM:        +
+    +                                             +
+    +            PARA MAÇÃ USE --> MACA           +
+    +            PARA MAMÃO USE --> MAMAO         +
+    +            PARA MELÃO USE --> MELAO         +
+    +            PARA MELÂNCIA USE --> MELANCIA   +
+    + +++++++++++++++++++++++++++++++++++++++++++ +
     `);
 
 frutas = prompt('Digite a fruta que você deseja: ')
 
 switch(frutas.toLowerCase()) {
     case 'maca':
-        fruta = 'maçã'
         console.log(`
-O preço da ${fruta} está R$` + preco.maca.toFixed(2) + '/Kg')
+O preço da maçã está R$` + preco.maca.toFixed(2) + '/Kg')
         qtd = parseInt(prompt(`
-Quantas unidades de ${fruta} você quer? Digite apenas o número: `))
-        peso_fruta = peso.maca
-        preco_fruta = preco.maca
-        peso_total = peso_fruta * qtd
-        preco_total = preco_fruta * peso_total
+Quantas maçãs você deseja? Digite apenas o número: `))
+        peso_total = peso.maca * qtd
+        preco_total = preco.maca * peso_total
+        console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+        prompt(`
+Aperte ENTER para continuar. `)
         break;
 
     case 'banana':
-
         console.log(`
-O preço da banana está R$` + preco.banana + '/Kg')
+O preço da banana está R$` + preco.banana.toFixed(2) + '/Kg.')
+        qtd = parseInt(prompt(`
+Quantas você gostaria? Digite apenas o número: `))
+        peso_total = peso.banana * qtd
+        preco_total = preco.banana * peso_total
+        console.log(`
+Balança: ` + peso_total.toFixed(3) + `/Kg.`)
+        break;
 
+        case 'laranja':
+        console.log(`
+O preço da laranja está R$` + preco.laranja.toFixed(2) + `/Kg.`)
+        qtd = parseInt(prompt(`
+Quantas laranjas você quer? Digite apenas o número: `))
+        peso_total = peso.banana * qtd
+        preco_total = preco.banana * peso_total
+        console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+        prompt(`
+Aperte ENTER para continuar. `)
+        break;
+
+        case 'manga':
+            console.log(`
+O preço da manga está R$` + preco.manga.toFixed(2) + `/Kg.`)
+            qtd = parseInt(prompt(`
+Quantas você deseja? Digite apenas o número: `))
+            peso_total = peso.manga * qtd
+            preco_total = preco.manga * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
+
+        case 'mamao':
+            console.log(`
+O preço do mamão está R$` + preco.mamao.toFixed(2) + `/Kg.`)
+            qtd = parseInt(prompt(`
+Quantas unidades você deseja? Digite apenas o número: `))
+            peso_total = peso.mamao * qtd
+            preco_total = preco.mamao * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
+
+        case 'melancia':
+            console.log(`
+O preço da melância está R$` + preco.melancia + `/Kg.`)
+            qtd = parseInt(prompt(`
+Quantas melâncias você deseja?`))
+            peso_total = peso.melancia * qtd
+            preco_total = preco.melancia * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
+
+        case 'abacaxi':
+            console.log(`
+O preço do abacaxi está R$` + preco.abacaxi.toFixed(2) + `Kg.`)
+            qtd = parseInt(prompt(`
+Quantas unidades você deseja? Digite apenas o número: `))
+            peso_total = peso.abacaxi * qtd
+            preco_total = preco.abacaxi * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
+
+        case 'pera':
+            console.log(`
+O preço da pera está R$` + preco.pera.toFixed(2) + `/Kg`)
+            qtd = parseInt(prompt(`
+Quantas unidades de pera você deseja? Digite apenas o número: `))
+            peso_total = peso.pera * qtd
+            preco_total = preco.pera * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
+
+        case 'uva':
+            console.log(`
+[AVISO] AS UVAS SÃO VENDIDAS POR BANDEJA.
+O preço da bandeja da uva está R$` + preco.uva.toFixed(2) `.`)
+            qtd = parseInt(prompt(`
+Quantas bandejas você deseja? Digite apenas o número: `))
+            preco_total = preco.uva * qtd
+            break;
+
+        case 'melao':
+            console.log(`
+O preço do melão está R$` + preco.melao + `/Kg.`)
+            qtd = parent(prompt(`
+Quantos melões você deseja? Digite apenas o número: `))
+            peso_total = peso.melao * qtd
+            preco_total = preco.melao * peso_total
+            console.log(`
+Peso total: ` + peso_total.toFixed(3) + `/Kg.`)
+            prompt(`
+Aperte ENTER para continuar. `)
+            break;
 
     default:
         console.log(`
-Fruta não encontrada ou em falta no estoque.`)
-}
-
-
-console.log(`
-O preço total ficou R$` + preco_total.toFixed(2) + `
+Fruta não encontrada ou em falta no estoque.
 `)
+
+}
+if (preco_total === undefined){
+
+}else {
+    console.log(`
+O preço total ficou R$` + preco_total.toFixed(2) + `.` + `
+`)
+    }
 
